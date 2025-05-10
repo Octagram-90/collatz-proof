a375a0e0eebdfca1d14f15180fc2a4b33d8ea0fb
Octagram-90 <diaadeek90@gmail.com>
2025-05-10 08:56:15 +0200

    UPDATE Z

--- modular_descent.v
+++ modular_descent.v
@@ -1,113 +1,22 @@
-Require Import Coq.Arith.Arith Lia.
+Require Import Coq.Arith.Arith Coq.Lists.List.
+From Stdlib Require Lia.
 Require Import CollatzProof.collatz_base.
+Import ListNotations.
 
-(* List of odd residues modulo 32 *)
 Definition odd_residues := [1; 3; 5; 7; 9; 11; 13; 15; 17; 19; 21; 23; 25; 27; 29; 31].
 
-(* Lemma: For each odd residue r mod 32, there exists k such that iterate_C k n reaches a smaller number *)
+Lemma iterate_C_decreases_odd : forall n r, In r odd_residues -> n mod 32 = r ->
+  exists k m, iterate_C k n = m /\ m < n.
+Proof.
+  intros n r Hr Hmod.
+  (* Placeholder: Generalize for odd residues *)
+  destruct Hr; subst; try (exists 1, (3 * n + 1); split; [reflexivity | lia]).
+  (* Handle remaining cases *)
+Admitted.
+
 Lemma modular_descent : forall r, In r odd_residues -> forall n, n mod 32 = r ->
   exists k m, iterate_C k n = m /\ m < n.
 Proof.
-  intros r Hr n Hn.
-  induction n as [|n' IH] using nat_ind.
-  - exists 0, 0. split; [reflexivity | lia].
-  - destruct Hr as [Hr | [Hr | [Hr | [Hr | [Hr | [Hr | [Hr | [Hr |
-           [Hr | [Hr | [Hr | [Hr | [Hr | [Hr | [Hr | [Hr | ]]]]]]]]]]]]]]]];
-    subst r.
-    + (* r = 1 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 4) = m /\ m < 3 * n' + 4).
-      { apply IH. replace (3 * n' + 4) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 3 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 10) = m /\ m < 3 * n' + 10).
-      { apply IH. replace (3 * n' + 10) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 5 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 16) = m /\ m < 3 * n' + 16).
-      { apply IH. replace (3 * n' + 16) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 7 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 22) = m /\ m < 3 * n' + 22).
-      { apply IH. replace (3 * n' + 22) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 9 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 28) = m /\ m < 3 * n' + 28).
-      { apply IH. replace (3 * n' + 28) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 11 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 34) = m /\ m < 3 * n' + 34).
-      { apply IH. replace (3 * n' + 34) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 13 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 40) = m /\ m < 3 * n' + 40).
-      { apply IH. replace (3 * n' + 40) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 15 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 46) = m /\ m < 3 * n' + 46).
-      { apply IH. replace (3 * n' + 46) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 17 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 52) = m /\ m < 3 * n' + 52).
-      { apply IH. replace (3 * n' + 52) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 19 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 58) = m /\ m < 3 * n' + 58).
-      { apply IH. replace (3 * n' + 58) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 21 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 64) = m /\ m < 3 * n' + 64).
-      { apply IH. replace (3 * n' + 64) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 23 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 70) = m /\ m < 3 * n' + 70).
-      { apply IH. replace (3 * n' + 70) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 25 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 76) = m /\ m < 3 * n' + 76).
-      { apply IH. replace (3 * n' + 76) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 27 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 82) = m /\ m < 3 * n' + 82).
-      { apply IH. replace (3 * n' + 82) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 29 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 88) = m /\ m < 3 * n' + 88).
-      { apply IH. replace (3 * n' + 88) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
-    + (* r = 31 *)
-      assert (H: exists k m, iterate_C k (3 * n' + 94) = m /\ m < 3 * n' + 94).
-      { apply IH. replace (3 * n' + 94) with (3 * (S n') + 1) by lia.
-        rewrite Hn. simpl. rewrite Nat.mod_small; lia. }
-      destruct H as [k [m [Hiter Hm]]].
-      exists (S k), m. split; [simpl; rewrite Hn, Nat.mod_small; lia | lia].
+  intros r Hr n Hmod.
+  apply iterate_C_decreases_odd; assumption.
 Qed.
--- stopping_time_finite.v
+++ stopping_time_finite.v
@@ -1,4 +1,6 @@
-Require Import CollatzProof.collatz_base CollatzProof.valuation_density Lia.
+Require Import Coq.Arith.Arith.
+From Stdlib Require Lia.
+Require Import CollatzProof.collatz_base.
 
 Fixpoint stopping_time_aux (n k max_k : nat) : nat :=
   if k >=? max_k then k
@@ -8,13 +10,23 @@ Fixpoint stopping_time_aux (n k max_k : nat) : nat :=
 Definition stopping_time (n : nat) : nat :=
   stopping_time_aux n 1 (16 * Nat.log2_up n).
 
-Lemma stopping_time_finite : forall n, n >= 2 -> iterate_C (stopping_time n) n < n.
+Lemma stopping_time_aux_terminates : forall n k max_k,
+  exists k', stopping_time_aux n k max_k = k' /\ k' <= max_k.
 Proof.
-  intros n Hn. unfold stopping_time, stopping_time_aux.
-  destruct (Nat.even n) eqn:Heven.
-  - unfold iterate_C, C. rewrite Heven. lia.
-  - pose proof (valuation_density n 16 Hn) as Hdens.
-    assert (Hprod: prod (map (fun i => (3 + 1 / INR (iterate_Psi i n)) / (2 ^ val2 (3 * iterate_Psi i n + 1))) (seq 0 16)) < 1).
-    { (* Explicit product *) lia. }
-    lia.
+  intros n k max_k. induction k; simpl.
+  - exists max_k; split; [reflexivity | lia].
+  - destruct (k >=? max_k) eqn:Hk; [exists k; split; [reflexivity | lia] |].
+    destruct (iterate_C k n <? n) eqn:Hn; [exists k; split; [reflexivity | lia] |].
+    apply IHk.
 Qed.
+
+Lemma stopping_time_finite : forall n, n >= 2 ->
+  iterate_C (stopping_time n) n < n.
+Proof.
+  intros n Hn. unfold stopping_time.
+  generalize dependent (16 * Nat.log2_up n).
+  intros max_k. induction max_k; simpl.
+  - lia.
+  - destruct (iterate_C 1 n <? n) eqn:Hn; [lia |].
+    apply IHmax_k; lia.
+Admitted.
