e5c83f9b44ca6e2a85fff10c0a3116f2bb617f3d
Octagram-90 <diaadeek90@gmail.com>
2025-05-10 02:29:47 +0200

    update

--- collatz_base.v
+++ collatz_base.v
@@ -1,8 +1,29 @@
 Require Import Coq.Arith.Arith Coq.Lists.List Coq.Reals.Reals.
 Import ListNotations.
 
-Fixpoint val2 (n : nat) : nat :=
-  if n =? 0 then 0 else if Nat.even n then S (val2 (n / 2)) else 0.
+(* Well-founded relation for val2 *)
+Definition val2_decreases (n m : nat) : Prop := m = n / 2 /\ Nat.even n = true /\ n > 0.
+
+Lemma val2_wf : well_founded val2_decreases.
+Proof.
+  unfold well_founded. intros n.
+  induction n as [|n' IH] using nat_ind.
+  - constructor. intros m [Hm _]. lia.
+  - constructor. intros m [Hm [Heven Hpos]].
+    assert (m < S n') by (rewrite Hm; apply Nat.div_lt; lia).
+    apply IH; lia.
+Qed.
+
+(* Definition of val2 using Function *)
+Function val2 (n : nat) {wf val2_wf n} : nat :=
+  if n =? 0 then 0
+  else if Nat.even n then S (val2 (n / 2))
+  else 0.
+Proof.
+  - intros n Hn Heven. unfold val2_decreases.
+    split; [reflexivity | split; [assumption | lia]].
+  - apply val2_wf.
+Defined.
 
 Definition C (n : nat) : nat :=
   if Nat.even n then n / 2 else 3 * n + 1.
