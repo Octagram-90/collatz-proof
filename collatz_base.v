e644e6e6d9735e53e14769bf7c75a2d4c4a416ce
Octagram-90 <diaadeek90@gmail.com>
2025-05-10 04:17:18 +0200

    update

--- collatz_base.v
+++ collatz_base.v
@@ -10,7 +10,7 @@ Proof.
   induction n as [|n' IH] using nat_ind.
   - constructor.
     intros m [_ [_ Hpos]].
-    exfalso; apply (Nat.lt_irrefl 0 Hpos).
+    contradiction Hpos.
   - constructor.
     intros m [Hm [Heven Hpos]].
     assert (m < S n') by (rewrite Hm; apply Nat.div_lt; auto with arith).
