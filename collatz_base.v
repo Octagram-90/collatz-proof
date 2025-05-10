afd32d140fac30f2ede4f9abb40586dae806b841
Octagram-90 <diaadeek90@gmail.com>
2025-05-10 03:52:26 +0200

    update

--- collatz_base.v
+++ collatz_base.v
@@ -6,37 +6,37 @@ Definition val2_decreases (n m : nat) : Prop := m = n / 2 /\ Nat.even n = true /
 
 Lemma val2_wf : well_founded val2_decreases.
 Proof.
-  unfold well_founded. intros n.
-  induction n as [|n' IH] using nat_ind.
-  - constructor.
+  unfold well_founded. intros n.
+  induction n as [|n' IH] using nat_ind.
+  - constructor.
     intros m [_ [_ Hpos]].
     lia.
-  - constructor.
+  - constructor.
     intros m [Hm [Heven Hpos]].
     assert (m < S n') by (rewrite Hm; apply Nat.div_lt; lia).
-    apply IH with m; lia.
+    apply IH; lia.
 Qed.
 
 (* Definition of val2 using Function *)
 Function val2 (n : nat) {wf val2_wf n} : nat :=
-  if n =? 0 then 0
-  else if Nat.even n then S (val2 (n / 2))
-  else 0.
+  if n =? 0 then 0
+  else if Nat.even n then S (val2 (n / 2))
+  else 0.
 Proof.
-  - intros n Hn Heven. unfold val2_decreases.
-    split; [reflexivity | split; [assumption | lia]].
-  - apply val2_wf.
+  - intros n Hn Heven. unfold val2_decreases.
+    split; [reflexivity | split; [assumption | lia]].
+  - apply val2_wf.
 Defined.
 
 Definition C (n : nat) : nat :=
-  if Nat.even n then n / 2 else 3 * n + 1.
+  if Nat.even n then n / 2 else 3 * n + 1.
 
 Fixpoint iterate_C (k n : nat) : nat :=
-  match k with 0 => n | S k' => C (iterate_C k' n) end.
+  match k with 0 => n | S k' => C (iterate_C k' n) end.
 
 Definition Psi (n : nat) : nat :=
-  if Nat.even n then n / (2 ^ val2 n) else
-    let m := 3 * n + 1 in m / (2 ^ val2 m).
+  if Nat.even n then n / (2 ^ val2 n) else
+    let m := 3 * n + 1 in m / (2 ^ val2 m).
 
 Fixpoint iterate_Psi (k n : nat) : nat :=
-  match k with 0 => n | S k' => Psi (iterate_Psi k' n) end.
+  match k with 0 => n | S k' => Psi (iterate_Psi k' n) end.
